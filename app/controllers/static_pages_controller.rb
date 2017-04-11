711e10eb6de42a03dc1b00e2f3dfb93a199e855c
Kevin Finley <kftwotwo@gmail.com>
2017-04-11 16:46:32 -0700

    add form/footer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
 
   def index
+    @contact_kevin = Contact.new
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :static_pages
+  resources :contacts, only: [:new, :create]
 end
