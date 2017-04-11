ab433f2f64c731dcee2079ee5ef25f931cc34e8c
Kevin Finley <kftwotwo@gmail.com>
2017-04-11 16:54:46 -0700

    add validation for presence of

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ApplicationRecord
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :email
+  validates_presence_of :description
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Contact, type: :model do
   it { should have_db_column(:company) }
   it { should have_db_column(:email) }
   it { should have_db_column(:description) }
+
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:description) }
 end
