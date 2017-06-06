0f94bc71e8342ded7e85aeac0af5d4c95def5827
Kevin Finley <kftwotwo@gmail.com>
2017-06-06 02:23:34 -0700

    allow admin to create a blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,4 +8,25 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      flash[:success] = "Successfully Created!"
+      redirect_to blog_path(@blog.id)
+    else
+      render :new
+      flash[:error] = "Hmmm... Somthing went wrong."
+    end
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :body)
+  end
 end
